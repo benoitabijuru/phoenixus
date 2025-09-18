5bd4a9e66517364cea1e15b0b64d4c9cf349ec26
benoitabijuru <abinoit2@gmail.com>
2025-09-18 11:22:14 +0200

    build project

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,18 +1,13 @@
 // app/dashboard/layout.tsx
 import { UserButton } from '@clerk/nextjs'
-import { auth } from '@clerk/nextjs/server'
-import { redirect } from 'next/navigation'
+
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  const { userId } = auth()
   
-  if (!userId) {
-    redirect('/sign-in')
-  }
 
   return (
     <div className="min-h-screen bg-gray-900">
